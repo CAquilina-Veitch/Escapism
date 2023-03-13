4ecf8e1529cc84e32f017a43d5926f00b9b5a24b
Cody <1027166@student.sae.edu.au>
2023-03-13 14:27:41 +0800

    Rework in progress

--- Assets/CutsceneManager.cs
+++ Assets/CutsceneManager.cs
@@ -1,36 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+
+
+[Serializable]
+public struct Cutscene
+{
+    public int id;
+    public float duration;
+    public Animator[] animators;
+    public GameObject[] objectsTurningOn;
+    public GameObject[] objectsTurningOff;
+    public GameObject[] prefabsToBeInstantiated;
+    public Vector3[] prefabPositions;
+}
 
 
 
 
 public class CutsceneManager : MonoBehaviour
 {
+    public Cutscene[] cutscenes;
+
     int currentCutscene;
-    
-    [SerializeField] float[] cutsceneDurations;
-    [SerializeField] Animator[] cutsceneAnimations;
-
-    [Header("Data that will be used to be set in script")]
-    [SerializeField] GameObject[] cutsceneObjects;
-
-    [SerializeField] int[][] cutsceneObjectsOn;
-    [SerializeField] int[][] cutsceneObjectsOff;
-
-    [SerializeField] int[][] cutscenePrefabsInstantiate;
-    [SerializeField] Vector2[][] cutscenePrefabsPosition;
-      
-
-    [Header("Temporary storage to restore after cutscene")]
-    [SerializeField] int[] cutsceneObjectsWereOn;
-    [SerializeField] GameObject[] cutscenePrefabsInstantiated;
-
 
 
     public void PlayCutscene(int num)
     {
-
+        StartCoroutine(CutsceneProcess(cutscenes[num]));
     }
 
     public void PlayNextCutscene()
@@ -39,21 +38,61 @@ public class CutsceneManager : MonoBehaviour
         PlayCutscene(currentCutscene);
     }
 
-
-
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator CutsceneProcess(Cutscene cutscene)
     {
-        
-    }
+        Dictionary<GameObject, Vector3> initialPos;
+        List<GameObject> cutsceneObjectsWerentOn = new List<GameObject>();
+        List<GameObject> cutsceneObjectsWerentOff = new List<GameObject>();
+        foreach(GameObject obj in cutscene.objectsTurningOn)
+        {
+            if (!obj.activeSelf)
+            {
+                cutsceneObjectsWerentOn.Add(obj);
+            }
+        }
+        foreach(GameObject obj in cutscene.objectsTurningOff)
+        {
+            if (obj.activeSelf)
+            {
+                cutsceneObjectsWerentOff.Add(obj);
+            }
+        }
+        foreach(Animator anim in cutscene.animators)
+        {
+            anim.SetTrigger($"Cutscene_{cutscene}");
+        }
+        List<GameObject> cutscenePrefabsInstantiated = new List<GameObject>();
+        for (int i = 0; i<cutscene.prefabsToBeInstantiated.Length; i++)
+        {
+            
+            GameObject temp = Instantiate(cutscene.prefabsToBeInstantiated[i], cutscene.prefabPositions[i], Quaternion.identity);
+            if (temp.TryGetComponent<Animator>(out Animator _prefabAnim))
+            {
+                _prefabAnim.SetTrigger($"Cutscene_{cutscene}");
+            }
+            cutscenePrefabsInstantiated.Add(temp);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        yield return new WaitForSeconds(cutscene.duration);
+
+        foreach(GameObject obj in cutsceneObjectsWerentOn)
+        {
+            obj.SetActive(false);
+        }
+        foreach(GameObject obj in cutsceneObjectsWerentOff)
+        {
+            obj.SetActive(true);
+        }
+        foreach(Animator anim in cutscene.animators)
+        {
+            anim.SetTrigger($"Cutscene_End");
+        }
+
+        foreach(GameObject prefab in cutscenePrefabsInstantiated)
+        {
+            Destroy(prefab);
+        }
+
+
     }
 }
