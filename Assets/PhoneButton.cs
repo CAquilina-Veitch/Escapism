ad8b9311427ec31a72512a4228860a146c2b2e70
Cody <1027166@student.sae.edu.au>
2023-05-04 18:36:42 +0800

    Fix phone

--- Assets/PhoneButton.cs
+++ Assets/PhoneButton.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,6 +14,10 @@ public class PhoneButton : MonoBehaviour
     bool phoneUnchecked = true;
     float amt = 2;
 
+    public void OpenPhone()
+    {
+        phoneUnchecked = false;
+    }
 
     private void OnEnable()
     {
